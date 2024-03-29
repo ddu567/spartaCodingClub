cb4757b9f255d8d60607b67ff52933260271cd13
ddu <shbea0503@naver.com>
2024-03-29 14:31:42 +0900

    240329-1 - 작업 컴퓨터 변경

---
+++ Baekjoon/src/codingTest/Q24265.java
@@ -0,0 +1,25 @@
+package codingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q24265 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        /*
+        int sum = 0;
+        for (int i = 1; i <= n - 1; i++) { =>n-1번
+            for (int j = i+1; j <= n; j++) {
+                sum += i*j; =>
+            }
+        }
+        return sum;
+
+         */
+
+    }
+}
